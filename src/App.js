efb75d833e522fc2073e7de5c20997b0d2bcaf01
knightvertrag <amiapu1@gmail.com>
2020-07-26 04:08:58 +0530

    Added Cart component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Container } from "./components/Utilities/Container/Container";
+import classes from "./App.module.scss";
+import Cart from "./components/Cart/Cart";
 
 const App = () => {
-  return <div>Yolo</div>;
+  return (
+    <Container>
+      <header className={classes.header}>
+        <h1>Checkout</h1>
+      </header>
+      <main className={classes.main}>
+        <div className={classes.formContainer}>Form</div>
+        <div className={classes.cartContainer}>
+          <Cart />
+        </div>
+      </main>
+    </Container>
+  );
 };
 
 export default App;
