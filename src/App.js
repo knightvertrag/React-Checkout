a0e7142210e80857c7827915310956eab2376d95
knightvertrag <amiapu1@gmail.com>
2020-07-29 03:07:14 +0530

    Linked form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "./components/Utilities/Container/Container";
 import classes from "./App.module.scss";
 import Cart from "./components/Cart/Cart";
+import Form from "./components/Form/Form";
 
 const App = () => {
   return (
@@ -10,7 +11,9 @@ const App = () => {
         <h1>Checkout</h1>
       </header>
       <main className={classes.main}>
-        <div className={classes.formContainer}>Form</div>
+        <div className={classes.formContainer}>
+          <Form />
+        </div>
         <div className={classes.cartContainer}>
           <Cart />
         </div>
